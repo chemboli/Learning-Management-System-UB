ba84b34fae4e86dd0309e86cb6e35bd627a8f9e2
chemboli <Chemboliroy07@gmail.com>
2026-07-17 17:41:52 +0100

    fixed one issue

--- work/Smartlearning-Frontend/src/environments/environment.prod.ts
+++ work/Smartlearning-Frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // Replace with your deployed Render backend URL once you have it, e.g.
   // 'https://smartlearning-backend.onrender.com/api'
-  apiUrl: 'https://learning-management-system-ub.onrender.com'
+  apiUrl: 'https://learning-management-system-ub.onrender.com/api'
 };
